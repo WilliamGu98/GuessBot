fbf6e6fdb629bada27baf68c435ae8577ea30e2f
William Gu <williamgu2011@hotmail.com>
2018-12-28 16:41:28 -0800

    Added find match logic, needs snapshot functionality from firebase

--- GuessBotApp/src/pages/lookingForMatch.js
+++ GuessBotApp/src/pages/lookingForMatch.js
@@ -6,36 +6,112 @@ import {
   StyleSheet,
   Button
 } from "react-native";
+import firebase from "react-native-firebase";
 
 export default class Loading extends React.Component {
   state = {
     email: this.props.navigation.getParam("email", null),
     username: this.props.navigation.getParam("username", null),
-    numMatches: 1
+    numMatches: 1,
+    matchID: null
   };
 
   componentDidMount() {
-    //TODO: logic for finding match
-    
+    const { email, password } = this.state;
+    firebase
+      .firestore()
+      .collection("matchmaking")
+      .limit(1)
+      .get()
+      .then(querySnapshot => {
+        var matchFound = false;
+
+        querySnapshot.forEach(doc => {
+          matchID = doc.id;
+          //Add user to found match
+          matchFound = true;
+          var users = doc.data().users;
+          var numMatches = users.push(email); //should only push unique
+          firebase
+            .firestore()
+            .collection("matchmaking")
+            .doc(doc.id)
+            .update({ users: users })
+            .then(match =>
+              this.setState({
+                numMatches: numMatches,
+                matchID: doc.id
+              })
+            );
+        });
+
+        //Create a new match
+        if (!matchFound) {
+          var users = [email];
+          firebase
+            .firestore()
+            .collection("matchmaking")
+            .add({ users: users })
+            .then(match =>
+              this.setState({
+                numMatches: 1,
+                matchID: match.id
+              })
+            );
+        }
+
+        //Issue with this
+        /*
+        dbMatch.onSnapshot(function(entry) {
+          console.log("Current data: " + entry.data());
+        });
+        */
+        //console.log("matchID: " + matchID);
+      })
+      .catch(function(error) {
+        console.log("Error:" + error);
+      });
   }
 
-  render() {
-    const { email, username, numMatches } = this.state;
+  //Cancel matchmaking logic
+  leaveMatch = () => {
+    const { email, username, matchID } = this.state;
 
+    //Update matchID entry to remove this email, if empty delete the entry
+    firebase
+      .firestore()
+      .collection("matchmaking")
+      .doc(matchID)
+      .get()
+      .then(querySnapshot => {
+        var users = querySnapshot.data().users;
+        console.log("BEFORE: " + users);
+
+        users = users.filter(user => {
+          return user != email;
+        });
+
+        console.log ("AFTER: " + users);
+        //querySnapshot.update({ users: users });
+
+      });
+
+    /*
+    this.props.navigation.navigate("Main", {
+      username: username,
+      email: email
+    });*/
+  };
+
+  render() {
+    const { numMatches, matchID } = this.state;
     return (
       <View style={styles.container}>
         <Text>Looking for match...</Text>
-        <Text>{numMatches}/4 players found</Text>
+        <Text>{numMatches}/3 players found</Text>
+        <Text>Match ID: {matchID}</Text>
         <ActivityIndicator size="large" />
-        <Button
-          title="Cancel"
-          onPress={() =>
-            this.props.navigation.navigate("Main", {
-              username: username,
-              email: email
-            })
-          }
-        />
+        <Button title="Cancel" onPress={this.leaveMatch} />
       </View>
     );
   }
